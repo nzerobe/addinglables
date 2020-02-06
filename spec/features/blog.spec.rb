46d670637fa0d95e65383f9cf8f87646d9b92039
nzeroberto <nzeroberto@gmail.com>
2020-02-06 04:26:59 -0500

    finaishingmanyowithSpecfeatues

--- spec/features/blog.spec.rb
+++ spec/features/blog.spec.rb
@@ -6,83 +6,76 @@ RSpec.feature "Blog management function", type: :feature do
   # In scenario (alias of it), write the processing of the test for each item you want to check.
   background do
 
-  Blog.create!(title: 'test_blog_01', content: 'testtesttest', status: 'off')
-  Blog.create!(title: 'test_blog_02', content: 'samplesample', status: 'off')
+    user = User.create!(name: "admin", email: 'adminn@gmail.com',  password: 'admin0011')
+    Blog.create!(title: 'test_blog_01', content: 'testtesttest', status: 'Off', user: user)
+    Blog.create!(title: 'test_blog_02', content: 'samplesample', status: 'Off', user: user)
 
- 
-  visit blogs_path
+    visit  root_path
+    fill_in  'Email' ,  with: 'adminn@gmail.com'
+    fill_in  'Password' ,  with: 'admin0011'
+    click_on 'Log in'
+  end
 
-end
-
-scenario "Test blog list" do
-  # Create two tasks in advance to use in the task list test
- 
-end
-  
- scenario " Testing blog creation "  do
+  scenario " Testing blog creation "  do
     visit new_blog_path
     fill_in 'blog_title', with: 'testtesttest'
     fill_in 'blog_content', with: 'samplesample'
-   click_on 'Create Blog'
+    select('Pending', from: 'blog[status]')
+    click_on 'Create Blog'
     expect(page).to have_content'testtesttest'
     expect(page).to have_content'samplesample'
   end
-  
+
   scenario " Testing Blog Details "  do
     blog = Blog.create!(title:"testtesttest",content:"samplesample",status: 'off')
     visit blog_path(blog.id)
     expect(page).to have_content "samplesample"
   end
-  
-  scenario "Test whether blogs are arranged in descending order of creation date" do
- 
-    visit root_path
-    
-#     
-     expect(Blog.order("updated_at desc").map(&:id)).to eq [ 10, 9]
 
+  scenario "Test whether blogs are arranged in descending order of creation date" do
+    blog1 = Blog.first
+    blog2 = Blog.second
+    visit root_path
+    expect(Blog.order("updated_at desc").map(&:id)).to eq [blog2.id, blog1.id]
   end
-  
-   scenario " Blog completion deadline entry test "  do
+
+  scenario " Blog completion deadline entry test "  do
     visit new_blog_path
     fill_in 'blog_title', with: 'testtesttest'
     fill_in 'blog_content', with: 'samplesample'
-    fill_in 'blog_deadline', with: DateTime.now
+    fill_in 'blog_deadline', with: '2019-02-02'
+    select('Pending', from: 'blog[status]')
     click_on 'Create Blog'
     expect(page).to have_content '2019'
   end
-  
-   scenario "Blog search execution test by title" do
-    visit root_path
-    fill_in 'title', with: '01'
-    click_on 'search'
-    expect(page).to have_content 'samplesample'
 
+  scenario "Blog search execution test by title" do
+    visit blogs_path
+    fill_in 'blog[title]', with: 'test'
+    click_on 'Search'
+    sleep 2
+    expect(page).to have_content 'testtesttest'
   end
 
   scenario "Blog search execution test by status" do
     visit root_path
-    fill_in 'title', with: ''
-    select 'Off', from: 'status'
-    click_on 'search'
-    expect(page).to have_content 'samplesample'
-
+    fill_in 'blog_title', with: ''
+    select 'Off', from: 'blog[status]'
+    click_on 'Search'
+    expect(page).to have_content 'testtesttest'
   end
 
   scenario "Blog search execution test by title and status " do
     visit root_path
-    fill_in 'title', with: 'test_'
-    select 'Off', from: 'status'
-    click_on 'search'
+    fill_in 'blog_title', with: 'test_'
+    select 'Off', from: 'blog[status]'
+    click_on 'Search'
     expect(page).to have_content 'samplesample'
-
   end
 
   scenario "Priority descending sort test " do
     visit root_path
-    click_on 'Sort in order of priority'
-     expect(Blog.order("priority ASC").each)
+    click_on 'Sort By Priority'
+    expect(Blog.order("priority ASC").each)
   end
-
-  
 end
